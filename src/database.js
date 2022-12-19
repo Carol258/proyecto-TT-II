0e14965b55654435af755db31cf2edaea3f5fb88
TT-2021-B061 <tt2021.b061@gmail.com>
2022-12-19 04:24:09 -0600

    Conexión a la base de datos en mongoDB Atlas (en la nube)

--- src/database.js
+++ src/database.js
@@ -1,12 +1,12 @@
 // CONEXIÓN A LA BASE DE DATOS
 const mongoose = require('mongoose');
 
-const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env;
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`
+// const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env;
+// const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true
 })
-    .then(db => console.log("Database is connected"))
+    .then(db => console.log("Database is connected with Atlas"))
     .catch(err => console.log(err))
